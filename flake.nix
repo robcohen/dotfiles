5a8d0fbdbdae54580e2d91e6b3050315f2051789
Your Name <.>
2024-01-09 09:33:56 -0600

    Yep

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hardware.url = "github:nixos/nixos-hardware";
     sops-nix.url = "github:Mic92/sops-nix";
-    ags.url = "github:Aylur/ags";
   };
 
   outputs = {
