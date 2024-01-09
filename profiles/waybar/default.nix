3762d7014d972233ad8b657451b41e399f819d60
Your Name <.>
2024-01-08 21:32:37 -0600

    fix

--- profiles/waybar/default.nix
+++ profiles/waybar/default.nix
@@ -12,9 +12,6 @@
           layer = "top";
           position = "top";
           height = 30;
-          output = [
-            "DP-1"
-          ];
           modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
           modules-center = [ "sway/window" "custom/hello-from-waybar" ];
 
