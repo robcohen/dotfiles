e8b59899c2f37683e22254e81271f16fc04dca9e
robcohen <robcohen@users.noreply.github.com>
2024-01-09 16:06:06 -0500

    fix

---
+++ hosts/brix/configuration.nix
@@ -0,0 +1,109 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+
+  imports = [
+    ./hardware-configuration.nix
+    ../ledger.nix
+  ];
+
+  nixpkgs = {
+    overlays = [
+    ];
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  # This will add each flake input as a registry
+  # To make nix3 commands consistent with your flake
+  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    auto-optimise-store = true;
+  };
+  
+  ## Networking
+  networking.hostName = "brix";
+  networking.networkmanager.enable = true;
+  
+  time.timeZone = "America/Chicago";
+
+  ## Sound
+
+  #sound.enable = true;
+  #hardware.pulseaudio.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+  # Boot Parameters
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+  programs.fish.enable = true;
+  
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 32*1024;
+  } ];
+
+  users.users = {
+    user = {
+      isNormalUser = true;
+      shell = pkgs.fish;
+      extraGroups = ["wheel" "networkmanager" "input" "video" "libvirtd"];
+    };
+  };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd sway";
+        user = "greeter";
+      };
+    };
+  };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  services.dbus.enable = true;
+
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.user.enableGnomeKeyring = true;
+
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    git
+  ];
+  
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.11";
+}
