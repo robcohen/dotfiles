cf5715ebf5b626a44c7d1fb7fc6de6378323e865
Your Name <.>
2024-01-09 14:45:26 -0600

    fix

--- profiles/git/default.nix
+++ profiles/git/default.nix
@@ -13,8 +13,8 @@ programs.git = {
     package = pkgs.gitAndTools.gitFull; # Install git wiith all the optional extras
     userName = "robcohen";
     userEmail = "robcohen@users.noreply.github.com";
-    signing.key = "";
-    signing.signByDefault = true;
+#    signing.key = "";
+#    signing.signByDefault = true;
     extraConfig = {
       core.editor = "hx";
       # Cache git credentials for 15 minutes
