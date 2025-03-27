b74e213a481df7dcca17d8ea816a3ad2565c5013
robcohen <robcohen@users.noreply.github.com>
2025-03-26 19:57:34 -0500

    no more ff

--- profiles/mimeapps.nix
+++ profiles/mimeapps.nix
@@ -2,7 +2,7 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "text/html" = "firefox.desktop";
+      "text/html" = "chromium.desktop";
       "x-scheme-handler/http" = "chromium.desktop";
       "x-scheme-handler/https" = "chromium.desktop";
       "x-scheme-handler/about" = "chromium.desktop";
